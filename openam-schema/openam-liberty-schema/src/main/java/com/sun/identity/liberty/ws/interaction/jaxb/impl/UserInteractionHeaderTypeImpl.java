490cda59e9caa1fb786dfcea7ff7c49ec1939c7c
Jan Fyrbach <jan.fyrbach@orchitech.cz>
2022-12-08 16:10:00 +0100

    Upgrade JAXB

--- openam-schema/openam-liberty-schema/src/main/java/com/sun/identity/liberty/ws/interaction/jaxb/impl/UserInteractionHeaderTypeImpl.java
+++ openam-schema/openam-liberty-schema/src/main/java/com/sun/identity/liberty/ws/interaction/jaxb/impl/UserInteractionHeaderTypeImpl.java
@@ -39,7 +39,7 @@ public class UserInteractionHeaderTypeImpl implements com.sun.identity.liberty.w
 
     public boolean isRedirect() {
         if (!has_Redirect) {
-            return javax.xml.bind.DatatypeConverter.parseBoolean(com.sun.xml.bind.DatatypeConverterImpl.installHook("false"));
+            return false;
         } else {
             return _Redirect;
         }
