09909808389e5161012975488c182be38adbd3d3
Jan Fyrbach <jan.fyrbach@orchitech.cz>
2024-06-07 00:17:48 +0200

    Fix CREST action authorization
    The token ID can be retrieved from HTTP header or cookie for
    authorization verification of CREST action.
    
    This fixes an issue when requesting of the session info without token ID
    in URL was forbidden - requests ended with HTTP status 403.

--- openam-core-rest/src/test/java/org/forgerock/openam/core/rest/session/TokenOwnerAuthzModuleTest.java
+++ openam-core-rest/src/test/java/org/forgerock/openam/core/rest/session/TokenOwnerAuthzModuleTest.java
@@ -1,18 +1,19 @@
 /*
-* The contents of this file are subject to the terms of the Common Development and
-* Distribution License (the License). You may not use this file except in compliance with the
-* License.
-*
-* You can obtain a copy of the License at legal/CDDLv1.0.txt. See the License for the
-* specific language governing permission and limitations under the License.
-*
-* When distributing Covered Software, include this CDDL Header Notice in each file and include
-* the License file at legal/CDDLv1.0.txt. If applicable, add the following below the CDDL
-* Header, with the fields enclosed by brackets [] replaced by your own identifying
-* information: "Portions copyright [year] [name of copyright owner]".
-*
-* Copyright 2015-2016 ForgeRock AS.
-*/
+ * The contents of this file are subject to the terms of the Common Development and
+ * Distribution License (the License). You may not use this file except in compliance with the
+ * License.
+ *
+ * You can obtain a copy of the License at legal/CDDLv1.0.txt. See the License for the
+ * specific language governing permission and limitations under the License.
+ *
+ * When distributing Covered Software, include this CDDL Header Notice in each file and include
+ * the License file at legal/CDDLv1.0.txt. If applicable, add the following below the CDDL
+ * Header, with the fields enclosed by brackets [] replaced by your own identifying
+ * information: "Portions copyright [year] [name of copyright owner]".
+ *
+ * Copyright 2015-2016 ForgeRock AS.
+ * Portions copyright 2024 Wren Security.
+ */
 package org.forgerock.openam.core.rest.session;
 
 import static org.forgerock.util.test.assertj.AssertJPromiseAssert.*;
@@ -64,7 +65,7 @@ public class TokenOwnerAuthzModuleTest {
         given(mockConfig.get()).willReturn(mockService);
         mockContext = setupUser("universal_id");
 
-        testModule = new TokenOwnerAuthzModule("tokenId", mockTokenManager, "deleteProperty");
+        testModule = new TokenOwnerAuthzModule(mockTokenManager, "deleteProperty");
     }
 
     @Test
