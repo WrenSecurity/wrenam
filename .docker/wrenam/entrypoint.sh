1c042541d9e2fb9a66b0ea2ace18f77e75feaa50
Pavel Horal <pavel.horal@orchitech.cz>
2025-07-24 11:19:05 +0200

    Improve Docker container initialization
    * custom entrypoint with server initialization
    * better environment variable naming and handling
    * global ssoadm wrapper script
    * sample configuration

---
+++ .docker/wrenam/entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -eu
+#
+# The contents of this file are subject to the terms of the Common Development and
+# Distribution License (the License). You may not use this file except in compliance with the
+# License.
+#
+# You can obtain a copy of the License at legal/CDDLv1.1.txt. See the License for the
+# specific language governing permission and limitations under the License.
+#
+# When distributing Covered Software, include this CDDL Header Notice in each file and include
+# the License file at legal/CDDLv1.1.txt. If applicable, add the following below the CDDL
+# Header, with the fields enclosed by brackets [] replaced by your own identifying
+# information: "Portions copyright [year] [name of copyright owner]".
+#
+# Copyright 2025 Wren Security
+#
+
+if [[ ! -z "${WRENAM_INIT_DISABLE:-}" ]]; then
+  # No initialization allowed, pass the control to Tomcat
+  exec "@"
+else
+  # Run background configuration script
+  /opt/wrenam/config.sh &
+  # Continue with the standard Tomcat startup
+  "$@"
+fi
